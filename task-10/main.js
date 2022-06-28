49d5d46ce080beea0e3ac702274529de69ada9f4
KDilara <106311483+KDilara@users.noreply.github.com>
2022-06-28 09:41:30 +0400

    Update main.js

--- task-10/main.js
+++ task-10/main.js
@@ -3,7 +3,24 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+
+   let input = result.input; // "5,2 4 7 12 9"
+   let input_data = input.split(','); // ['5', '2 4 7 12 9']
+   let n = parseInt(input_data[0]); // Massivin element sayı. Nümunə - 5
+   let arr = input_data[1].split(' '); // Daxil edilmiş massiv. Nümunə - ['2', '4', '7', '12', '9']
+   let max = 0;
+   let second = 0;
+   for (i = 0; i < arr.length; i++) {
+     if (arr[i] > max) {
+       max = arr[i];
+     }
+    
+   }
+   for (i = 0; i < arr.length; i++) {
+     if (arr[i] > second && arr[i]!=max) {
+       second = arr[i];
+     }
+    
+   }
+   console.log(second+max)
 });
